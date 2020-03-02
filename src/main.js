12fc7fe18a2f83ed8b3000cd676afbf662335c7b
18627805032@163.com <18627805032@163.com>
2020-03-02 20:24:00 +0800

    解决移动端点击延迟

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index.js'
 import toast from 'components/common/toast'
+// 解决移动端300 延迟
+import FastClick from 'fastclick'
 Vue.config.productionTip = false
 // 给$bus赋值vue实例才能发射事件
 // 兄弟间的事件传递
@@ -10,6 +12,7 @@ Vue.prototype.$bus=new Vue()
 //安装toast插件
 //安装好后,就会调用toast里install方法
 Vue.use(toast)
+FastClick.attach(document.body)
 new Vue({
   router,
   store,
