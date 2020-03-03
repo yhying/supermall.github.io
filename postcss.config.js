f00a4a97f48054cc5a8890073db2d7385b66a7a2
18627805032@163.com <18627805032@163.com>
2020-03-03 18:04:48 +0800

    定义规则

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,21 @@
-
 module.exports = {
-    plugins: {
-      autoprefixer: {},
-      'postcss-px-to-viewport': {
-         viewportWidth: 375,   // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
-        viewportHeight: 667, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
-        unitPrecision: 5,     // 指定`px`转换为视窗单位值的小数位数
-        viewportUnit: "vw",   //指定需要转换成的视窗单位，建议使用vw
-        selectorBlackList: ['.ignore','tab-bar','tab-bar-item'],// 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
-        minPixelValue: 1,     // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
-        mediaQuery: false     // 允许在媒体查询中转换`px`
-      }
+  plugins: {
+    autoprefixer: {},
+    'postcss-px-to-viewport': {
+      viewportWidth: 375, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
+      viewportHeight: 667, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
+      unitPrecision: 5, // 指定`px`转换为视窗单位值的小数位数
+      viewportUnit: "vw", //指定需要转换成的视窗单位，建议使用vw
+      selectorBlackList: ['.ignore', 'tab-bar', 'tab-bar-item'], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+      minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+      mediaQuery: false, // 允许在媒体查询中转换`px`，
+    //   exclude: [/TabBar/] // 指定不转换的文件，  
     }
   }
+}
+//   1.在js中使用正则：/正则相关规则/
+//   2.exclude中存放的元素必须是正则表达式
+//   3.按照排除的文件写对应的正则：
+//   正则的规则：
+//   1>^abc:表示匹配的内容，必须以什么内容开头(以abc开头)
+//   2>$
